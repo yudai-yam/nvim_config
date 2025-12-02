9cc8bba661c2223a2a4c56a35a2b7befe95649e4
yudai-yam <yudaimadeinjapan@gmail.com>
2025-12-02 17:12:44 +0100

    add spell check for tex

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,18 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- This is defined at the top of /lazyvim/config/autocmds.lua
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Add tex in spell check
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("wrap_spell"),
+  pattern = { "tex", "text", "plaintex", "typst", "gitcommit", "markdown" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.spell = true
+  end,
+})
